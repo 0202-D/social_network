ba2ac23dd3c7d520dcfc56ab46e90b3dbff492f7
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-06-01 20:46:52 +0300

    add testCreateFriendClaim()

--- src/test/java/ru/effectivemobile/social_network/service/user/UserServiceImplTest.java
+++ src/test/java/ru/effectivemobile/social_network/service/user/UserServiceImplTest.java
@@ -1,29 +1,59 @@
 package ru.effectivemobile.social_network.service.user;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import ru.effectivemobile.social_network.dao.FriendRepository;
 import ru.effectivemobile.social_network.dao.UserRepository;
 import ru.effectivemobile.social_network.model.Friend;
+import ru.effectivemobile.social_network.model.User;
 
-import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static ru.effectivemobile.social_network.TestUtils.getUser;
+import static ru.effectivemobile.social_network.model.ClaimAsFriendStatus.NEW;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@WithMockUser
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
 
-    // Добавить тесты
+    @Mock
+    private UserRepository userRepository;
 
+    @Mock
+    private FriendRepository friendRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    public void testCreateFriendClaim() {
+        // Set up authentication context
+        User firstUser = getUser();
+        firstUser.setId(1L);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(firstUser, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Mock user repository
+        User secondUser = getUser();
+        secondUser.setId(2L);
+        Mockito.when(userRepository.findById(2L)).thenReturn(Optional.of(secondUser));
+
+        // Execute service method
+        userService.createFriendClaim(2L);
+
+        // Verify friend creation
+        ArgumentCaptor<Friend> friendArgumentCaptor = ArgumentCaptor.forClass(Friend.class);
+        Mockito.verify(friendRepository).save(friendArgumentCaptor.capture());
+        Friend friend = friendArgumentCaptor.getValue();
+        assertEquals(firstUser, friend.getFirstUser());
+        assertEquals(secondUser, friend.getSecondUser());
+        assertEquals(NEW, friend.getStatus());
+    }
 }
+
+
